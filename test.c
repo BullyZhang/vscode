8c58cfa3b6ca6df781eae82a0e9b5b54d5998c42
BullyZhang <694530725@qq.com>
2019-05-14 16:57:02 +0800

    11

--- test.c
+++ test.c
@@ -1,6 +1,21 @@
-#include<stdio.h>
+#include <stdio.h>
+#include<string.h>
+#include<stdlib.h>
+typedef union
+{
+    char c;
+    int d;
+    double e;
+} Test;
 
-int main(){
-    printf("hello world");
-    return 0;
+int get(int *c){
+    
+
+    return sizeof(c);
+}
+int main()
+{
+    int c[3] = {1,2,3};
+    printf("%d",sizeof(c));
+    return get(c);
 }
